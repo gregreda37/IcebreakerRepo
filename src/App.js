cef1b000dc2e0d73bbb380813f5caab0b8822bd5
Ahmed Elhendy <mini@Ahmeds-MacBook-Pro.local>
2020-03-23 18:08:18 -0400

    changed name of project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Greg's React Project
+          Felicity's React Project
         </p>
         <a
           className="App-link"
